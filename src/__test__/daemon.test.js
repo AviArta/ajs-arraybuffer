2e247aa3b4b40cca1a6c0bc0d3345e5ec3f27475
AviArta <buzlova.a@yandex.ru>
2024-06-03 17:06:26 +0300

    Init project

---
+++ src/__test__/daemon.test.js
@@ -0,0 +1,16 @@
+import Daemon from "../daemon";
+
+// eslint-disable-next-line no-undef
+test("check create object Daemon", () => {
+  const daemon = new Daemon("Cap");
+  const expectedValue = {
+    name: "Cap",
+    type: "Daemon",
+    health: 100,
+    level: 1,
+    attack: 10,
+    defence: 40,
+  };
+  // eslint-disable-next-line no-undef
+  expect(daemon).toEqual(expectedValue);
+});
---
+++ src/index.js
@@ -0,0 +1,3 @@
+import "./character";
+import "./magician";
+import "./daemon";
